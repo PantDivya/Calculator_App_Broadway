6f3b7a9b6406b063adac435b3e53aa349d79ce88
Satyam Karki <74590592+satyam-karki@users.noreply.github.com>
2024-03-28 19:00:23 +0545

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,15 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to Broadway Calculator");
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Welcome to Broadway Calculator");
 
-Console.WriteLine("Please provide the first input");
-int firstInput=int.Parse(Console.ReadLine());
+//Console.WriteLine("Please provide the first input");
+//int firstInput=int.Parse(Console.ReadLine());
 
 
-Console.WriteLine("Please provide the second  input");
+//Console.WriteLine("Please provide the second  input");
 
-int secondInput=int.Parse(Console.ReadLine());
+//int secondInput=int.Parse(Console.ReadLine());
 
-int sum=firstInput+secondInput;
-//string Interpolation
-Console.WriteLine($"The result is : {sum}");
+//int sum=firstInput+secondInput;
+////string Interpolation
+//Console.WriteLine($"The result is : {sum}");
 
