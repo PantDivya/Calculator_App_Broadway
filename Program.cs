72f121cd7248ab46255ca28ed25fab7b2f2c8e8c
PantDivya <divyapant108@gmail.com>
2024-04-17 12:36:42 +0545

    Basic operation added (sum, difference, multiply, division) using if else

--- Program.cs
+++ Program.cs
@@ -1,15 +1,46 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to Broadway Calculator");
+Console.WriteLine("Welcome to MyCalculator App");
+Console.WriteLine("Please provide any operation");
+Console.WriteLine("1. Addition");
+Console.WriteLine("2. Subtraction");
+Console.WriteLine("3. Multiplication");
+Console.WriteLine("4. Division");
 
-Console.WriteLine("Please provide the first input");
-int firstInput=int.Parse(Console.ReadLine());
+#region performOperation
 
-//statement
-Console.WriteLine("Please provide the second  input");
+int operation = int.Parse(Console.ReadLine());
+int result;
 
-int secondInput=int.Parse(Console.ReadLine());
+Console.WriteLine("Provide first input:");
+int first = int.Parse(Console.ReadLine());
 
-int sum=firstInput+secondInput;
-//string Interpolation
-Console.WriteLine($"The result is : {sum}");
 
+Console.WriteLine("Provide Second input:");
+int second = int.Parse(Console.ReadLine());
+
+if (operation == 1)
+{
+    result = first + second;
+    Console.WriteLine($"Sum : " + result);
+}
+else if (operation == 2)
+{
+    result = first - second;
+    Console.WriteLine($"Difference : " + result);
+}
+else if (operation == 3)
+{
+    result = first * second;
+    Console.WriteLine($"Product : " + result);
+}
+else if (operation == 4)
+{
+    result = first / second;
+    Console.WriteLine($"division : " + result);
+}
+else
+{
+    Console.WriteLine("Invalid Selection");
+}
+
+#endregion
